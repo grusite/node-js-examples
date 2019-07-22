c9d912b7dc019ae9f0a55a61c2f352928d0c873b
Jorge <grusite@gmail.com>
2019-07-22 22:49:30 +0200

    change ejs to html engine

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -11,7 +11,8 @@ var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("view engine", "html");
+app.engine("html", require("ejs").__express);
 
 // Todo lo que tiene use es un middleware y se ejecutan de uno en uno
 app.use(logger("dev"));
