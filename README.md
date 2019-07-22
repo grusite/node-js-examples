9676b9836032a3f6e08a637ae46fc791b4e2f314
Jorge <grusite@gmail.com>
2019-07-22 08:13:59 +0200

    added README and installed express-validator

---
+++ README.md
@@ -0,0 +1,19 @@
+# Módulo KC NodeJS
+
+Este repositorio ha sido usado tanto para seguir las clases durante las dos semanas como para realizar la práctica, usando el directorio _nodepop_
+
+## examples
+
+Usado para ilustrar los ejemplos del profesor durante las clases
+
+## nodeapi
+
+Usado para practicar con express, mongodb y sus configuraciones
+
+## nodepop
+
+Usado para la práctica entregable de NodeJS.
+
+> express nodepop -ejs
+> npm install mongoose
+> npm install eslint
--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const { query, validationResult } = require("express-validator");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+/* GET all advertisements. */
+router.get("/apiv1/anuncios", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* GET ads with filter. */
+router.get(
+  "/",
+  [
+    query("start").isNumeric(),
+    query("limit").isNumeric(),
+    query("sort").isAlphanumeric(),
+    query("tag").isAlphanumeric()
+  ],
+  (req, res, next) => {
+    res.render("index", { title: "Express" });
+  }
+);
 
 module.exports = router;
